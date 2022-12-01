bdfb50f52f1b9c96a194569696d0976b5f780ee1
kurageso <63407106+kurageso@users.noreply.github.com>
2022-12-01 21:32:44 +0900

    update jest config

--- plugin/neotest.lua
+++ plugin/neotest.lua
@@ -1,8 +1,8 @@
   require("neotest").setup({
     adapters = {
       require('neotest-jest')({
-        jestCommand = "yarn test --",
-        jestConfigFile = "custom.jest.config.ts",
+        jestCommand = "npm test --",
+        jestConfigFile = "jest.config.ts",
         env = { CI = true },
         cwd = function(path)
           return vim.fn.getcwd()
